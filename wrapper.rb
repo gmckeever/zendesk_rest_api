64972230f1caa475f30b22942240a64a074f0330
Greg McKeever <gmckeever@gmail.com>
2013-07-25 22:01:39 +0000

    add wrapper file

---
+++ wrapper.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+require 'zendesk_api'
+require 'pp'
+
+client = ZendeskAPI::Client.new do |config|
+      # Mandatory:
+    
+      config.url = "https://nitrousio.zendesk.com/api/v2" # e.g. https://mydesk.zendesk.com/api/v2
+    
+      config.username = ENV['ZENDESK_USERNAME']
+    
+      # Choose one of the following depending on your authentication choice
+      #config.token = ENV['ZENDESK_TOKEN']
+      config.password = ENV['ZENDESK_PASSWORD'] 
+    
+      # Optional:
+    
+      # Retry uses middleware to notify the user
+      # when hitting the rate limit, sleep automatically,
+      # then retry the request.
+      config.retry = true
+    
+      # Logger prints to STDERR by default, to e.g. print to stdout:
+      require 'logger'
+      config.logger = Logger.new(STDOUT)
+    
+      # Changes Faraday adapter
+      # config.adapter = :patron
+    
+      # Merged with the default client options hash
+      config.client_options = { :ssl => false }
+    
+      # When getting the error 'hostname does not match the server certificate'
+      # use the API at https://yoursubdomain.zendesk.com/api/v2
+end
+
+all_tickets = client.tickets.all?
+pp all_tickets
--- zendesk.rb
+++ zendesk.rb
@@ -1,27 +1,52 @@
 require 'rubygems'
 require 'httparty'
 require 'json'
+require 'crack'
 require 'pp'
 
 class Zendesk
-  attr_accessor :response
+	attr_accessor :response
 
-  include HTTParty
-  default_params :output => 'json'
-  format :json
+	include HTTParty
+	default_params :output => 'json'
+	format :json
   #base_uri = "https://nitrousio.zendesk.com/api/v2"
 
   def initialize
-    auth = {:username => ENV['ZENDESK_USERNAME'], :password => ENV['ZENDESK_PASSWORD']}
-    @response = HTTParty.get("https://nitrousio.zendesk.com/api/v2/tickets.json", 
-                     :basic_auth => auth)
+  	auth = {:username => ENV['ZENDESK_USERNAME'], :password => ENV['ZENDESK_PASSWORD']}
+  	@response = HTTParty.get("https://nitrousio.zendesk.com/api/v2/tickets.json", :basic_auth => auth)
   end
 
 end
+
+bug_hash = {}
+feedback_hash = {}
+issue_hash = {}
 
 zendesk = Zendesk.new.response
 #all_tags = zendesk.body["tags"].map
 
 parsed = JSON.parse(zendesk.body)
 
-#pp parsed
+pp parsed
+
+# parsed.each do |ticket|
+
+# 	name = tag.name
+	
+# 	if status = "pending"
+# 		if bug_hash.has_key?(name)
+# 			bug_hash[name] = bug_hash[name] + 1	
+# 		elsif feedback_hash.has_key?(name)
+# 			feedback_hash[name] = feedback_hash[name] + 1	
+# 		elsif issue_hash.has_key?(name)
+# 			issue_hash[name] = issue_hash[name] + 1	
+# 		elsif category = "bug"
+# 			bug_hash[tag] = 0
+# 		elsif category = "feedback"
+# 			feedback_hash[tag] = 0
+# 		elsif category = "issue"
+# 			issue_hash[tag] = 0
+# 		end
+# 	end
+# end
